4cfa73d4213e87f4ab2e518781157e8009a9952f
Willem Boshoff <willem.boshoff@dyna-mo.com>
2023-08-08 20:25:03 +0200

    Create seperate Postgres DB for each test.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
 use email_newsletter::configuration::get_configuration;
 use email_newsletter::startup::run;
+use sqlx::PgPool;
 use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection_pool = PgPool::connect(
+        &configuration.database.connection_string()
+    )
+        .await
+        .expect("Failed to connect to Postgres.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address).expect("Failed to bind to random port");
-    run(listener)?.await
+    run(listener, connection_pool)?.await
 }
