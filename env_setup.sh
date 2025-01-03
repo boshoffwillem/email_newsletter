463a34b7ed4e60e67b2573f0da142d7b78ddc3a7
Willem Boshoff <willem.boshoff@dyna-mo.com>
2025-01-03 14:35:19 +0200

    Finish implementing tracing

--- env_setup.sh
+++ env_setup.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
+rustup default nightly
 cargo install cargo-watch
-`cargo-watch` monitors your source code to trigger commands every time a file changes.
+# `cargo-watch` monitors your source code to trigger commands every time a file changes.
 cargo install cargo-tarpaulin
 rustup component add clippy
 rustup component add rustfmt
 cargo install cargo-audit
 cargo install cargo-expand
-cargo install --version="~0.6" sqlx-cli --no-default-features --features rustls,postgres
+cargo install --version="~0.8.1" sqlx-cli --no-default-features --features rustls,postgres
 cargo install cargo-deny
+cargo install cargo-udeps
 cargo deny check advisories
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,13 @@
 use email_newsletter::configuration::get_configuration;
 use email_newsletter::startup::run;
+use email_newsletter::telemetry;
 use sqlx::PgPool;
 use std::net::TcpListener;
-use tracing::subscriber::set_global_default;
-use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
-use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let env_filter = EnvFilter::try_from_default_env()
-        .unwrap_or_else(|_| EnvFilter::new("info"));
-    let formatting_layer = BunyanFormattingLayer::new(
-        "email_newsletter".into(),
-        std::io::stdout
-    );
-    let subscriber = Registry::default()
-        .with(env_filter)
-        .with(JsonStorageLayer)
-        .with(formatting_layer);
-    set_global_default(subscriber)
-        .expect("Failed to set global default subscriber");
+    let subscriber = telemetry::get_subscriber("email_newsletter".into(), "info".into());
+    telemetry::init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
